8851990c63f7fa59157af260a3b9dee50f91584b
Irs444 <irs786had123@gmail.com>
2024-08-09 23:53:09 +0530

    apply css

--- src/components/MainScreenStyle.jsx
+++ src/components/MainScreenStyle.jsx
@@ -22,19 +22,51 @@ module.exports = {
         fontSize: 40,
         textAlign: "right",
     },
-    main_screen_keypad:{
-        width:"100%",
-        height:"75%",
-        display:"flex",
+    main_screen_keypad: {
+        width: "100%",
+        height: "75%",
+        display: "flex",
     },
-    main_screen_keypad_row:{
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-between",
-        width:"100%",
-        alignItems:"center",
-        padding:10,
+    main_screen_keypad_row: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        alignItems: "center",
+        padding: 10,
+    },
+    btn_outer: {
+        width: 90,
+        height: 90,
+        backgroundColor: "#808D7C",
+        elevation: 10,
+        borderRadius:10,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    btn1_outer:{
+        width:90,
+        height:90,
+        backgroundColor:'#5F6F65',
+        borderRadius:50,
+        elevation:10,
+        alignItems:'center',
+        justifyContent:'center',
+        
+    },
+    bg1_button:{
+        color:"white",
+        fontSize:20,
+    },
+    bg2_button:{
+        color:"white",
+        fontSize:20,
+      
+       
     },
     
+
+    
+
 
 }
